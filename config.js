63da2365ad53915377a4d50fb0cb23b0b12d5b91
robertods <Matt@iMac-de-Matt.local>
2020-05-25 09:06:13 -0300

    Inicio

---
+++ config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAhoHFd-2Zquuz6pqmQXSKY82zzBFE4aEI",
+    authDomain: "cleantime-a4954.firebaseapp.com",
+    databaseURL: "https://cleantime-a4954.firebaseio.com",
+    projectId: "cleantime-a4954",
+    storageBucket: "cleantime-a4954.appspot.com",
+    messagingSenderId: "102738100289",
+    appId: "1:102738100289:web:568eee17c0cfd36bb2c6c0"
+};
+
+firebase.initializeApp(firebaseConfig);
